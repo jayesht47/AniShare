3f026e7371cc5394a1834ee4a326eaead8191bc4
Jayesh <jayeshthakur47@gmail.com>
2021-08-18 12:37:02 +0530

    http request local

--- src/App.js
+++ src/App.js
@@ -5,10 +5,64 @@ import ListSummary from "./Components/ListSummary";
 import Login from "./Components/Login/Login";
 import Header from "./Layout/Header";
 import AuthContext from "./Store/auth-context";
+import { useState,useCallback,useEffect } from "react";
+
 
 function App() {
 
   const authContextData = useContext(AuthContext);
+
+  const [animeList,setAnimeList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const fetchAnimeHandler = useCallback(async () => {
+    const userAnimeList = [6589, 294];
+
+    setIsLoading(true);
+    setError(null);
+
+    try {
+
+      const userAnimeListData = [];
+
+      for (const anime of userAnimeList) {
+        const response = await fetch(`http://localhost:9092/anime/${anime}`);
+        if (!response.ok) {
+          throw new Error("Something went wrong!");
+        }
+
+        const animeData = await response.json();
+
+        console.info(animeData);
+
+        userAnimeListData.push({
+          id: animeData.animeId,
+          imageURI: animeData.posterImage.tiny,
+          name: animeData.animeName,
+          status : animeData.status,
+          ageRating : animeData.ageRating,
+          episodeCount : animeData.episodeCount,
+          averageRating : animeData.rating
+
+        });
+
+        console.log(userAnimeListData);
+        setAnimeList(userAnimeListData);
+      }
+    } catch (error) {
+      setError(error.message);
+    }
+    setIsLoading(false);
+  }, []);
+
+  useEffect(() => {
+    fetchAnimeHandler();
+  }, [fetchAnimeHandler]);
+
+  let content = <p>No Anime Found</p>;
+
+  if(animeList.length > 0 ) content =  <AnimeGrid userAnimeListData = {animeList} userName = "Jayesh"/>
 
   return (
     <Fragment>
@@ -16,7 +70,7 @@ function App() {
       {! authContextData.isLoggedIn &&  <Login/>}
       {authContextData.isLoggedIn && <ListSummary userName = "Jayesh"/>}
       {/* {authContextData.isLoggedIn && <AnimeList userName = "Jayesh"/>} */}
-      {authContextData.isLoggedIn && <AnimeGrid userName = "Jayesh"/>}
+      {authContextData.isLoggedIn && content }
     </Fragment>
   );
 }
--- src/Components/Anime/AnimeGrid.js
+++ src/Components/Anime/AnimeGrid.js
@@ -1,53 +1,57 @@
 // import Card from "../../UI/Card";
+import { useCallback, useEffect, useState } from "react";
 import classes from "./AnimeGrid.module.css";
 import AnimeGridItem from "./AnimeGridItem";
 
-const DUMMY_DATA = [
-  {
-    name: "Sword Art Online",
-    malRating: 6,
-    malLink: "https://myanimelist.net/anime/11757",
-    id: 11757,
-  },
-  {
-    name: "Tensei shitara Slime Datta Ken 2nd Season Part 2",
-    malRating: 8.52,
-    malLink: "https://myanimelist.net/anime/41487/",
-    id: 41487,
-  },
-  {
-    name: "Tantei wa Mou, Shindeiru.",
-    malRating: 7.52,
-    malLink: "https://myanimelist.net/anime/46471/",
-    id: 4471,
-  },
-  {
-    name: "Tantei wa Mou, Shindeiru.",
-    malRating: 7.52,
-    malLink: "https://myanimelist.net/anime/46471/",
-    id: 46471,
-  },
-  {
-    name: "Tantei wa Mou, Shindeiru.",
-    malRating: 7.52,
-    malLink: "https://myanimelist.net/anime/46471/",
-    id: 4641,
-  },
-];
+// const DUMMY_DATA = [
+//   {
+//     name: "Sword Art Online",
+//     malRating: 6,
+//     malLink: "https://myanimelist.net/anime/11757",
+//     id: 11757,
+//   },
+//   {
+//     name: "Tensei shitara Slime Datta Ken 2nd Season Part 2",
+//     malRating: 8.52,
+//     malLink: "https://myanimelist.net/anime/41487/",
+//     id: 41487,
+//   },
+//   {
+//     name: "Tantei wa Mou, Shindeiru.",
+//     malRating: 7.52,
+//     malLink: "https://myanimelist.net/anime/46471/",
+//     id: 4471,
+//   },
+//   {
+//     name: "Tantei wa Mou, Shindeiru.",
+//     malRating: 7.52,
+//     malLink: "https://myanimelist.net/anime/46471/",
+//     id: 46471,
+//   },
+//   {
+//     name: "Tantei wa Mou, Shindeiru.",
+//     malRating: 7.52,
+//     malLink: "https://myanimelist.net/anime/46471/",
+//     id: 4641,
+//   },
+// ];
 
-const AnimeGrid = () => {
+const AnimeGrid = (props) => {
   return (
     <div className={classes.animeGrid}>
-        {DUMMY_DATA.map((element) => {
-          return (
-            <AnimeGridItem
-              key={element.id}
-              name={element.name}
-              malRating={element.malRating}
-              malLink={element.malLink}
-            />
-          );
-        })}
+      {props.userAnimeListData.map((element) => {
+        return (
+          <AnimeGridItem
+            key={element.id}
+            name={element.name}
+            imageURI={element.imageURI}
+            status = {element.status}
+            episodeCount = {element.episodeCount}
+            ageRating = {element.ageRating}
+            AverageRating = {element.averageRating}
+          />
+        );
+      })}
     </div>
   );
 };
